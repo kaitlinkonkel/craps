a72f27d60c31902ebde1ed07ce2a99dea4e8a3b1
kaitlin.konkel <kaitlin.konkel@MU-C1-07.mullen.pvt.k12.co.us>
2016-08-24 11:13:17 -0600

    initial commit

---
+++ craps/src/craps/craps.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class craps
+	{
+
+	public static void main(String[] args)
+			{
+				System.out.println("What is your name?");
+				Scanner userInput = new Scanner (System.in);
+				String name = userInput.nextLine();
+
+			}
+
+	}
